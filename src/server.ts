35781920fcc494487ae17fc2ae536e0cdc7fc84c
Will Cullen <itzcull@outlook.com>
2025-04-06 09:33:07 +1000

    more linting and type adjustments

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono<Env>()
 
 app.get('/', async (c) => {
-	c.text('Hello World')
+	return c.text('Hello World')
 })
 
 export default app
