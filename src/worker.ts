b2d8c45d132eebb6abc9e61631f168189a4de570
Will Cullen <itzcull@outlook.com>
2025-06-08 02:14:04 +1000

    clean

--- src/worker.ts
+++ src/worker.ts
@@ -1,13 +1,13 @@
-import { Hono } from 'hono'
 import { defineHandlers } from './utils'
 
 export default defineHandlers({
-	app: (app) => {
-		app.get('/', (c) => c.text('Hello, world!'))
-	},
-	queue(batch, _env, _ctx) {
+	app: (app) => app.get('/', (c) => c.text('Hello, world!')),
+	queue(batch) {
 		for (const message of batch.messages) {
 			console.log(message)
 		}
+	},
+	scheduled(controller) {
+		console.log(`Run at "${new Date(controller.scheduledTime).toISOString()}" with CRON "${controller.cron}"`)
 	}
 })
