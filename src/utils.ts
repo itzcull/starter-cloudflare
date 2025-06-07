4ae74ba02569ce36a46cf347e28d618504a2013e
Will Cullen <itzcull@outlook.com>
2025-06-08 02:12:13 +1000

    more updates

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,20 @@
 import { Hono } from 'hono'
 
 interface DefineHandlersOptions<Env, QueueMessage, CfHostMetadata> {
-	app: (app: Hono<Env>) => void
-	scheduled?: ExportedHandlerScheduledHandler<Env>
-	queue?: ExportedHandlerQueueHandler<Env, QueueMessage>
+  app: (app: Hono<Env>) => void
+  scheduled?: ExportedHandlerScheduledHandler<Env>
+  queue?: ExportedHandlerQueueHandler<Env, QueueMessage>
 }
 
 export function defineHandlers<Env, QueueMessage, CfHostMetadata>(
-	options: DefineHandlersOptions<Env, QueueMessage, CfHostMetadata>
+  options: DefineHandlersOptions<Env, QueueMessage, CfHostMetadata>
 ): ExportedHandler<Env, QueueMessage, CfHostMetadata> {
-	return {
-		...options,
-		fetch: (request, env, ctx) => {
-			const app = new Hono<Env>()
-			options.app(app)
-			return app.fetch(request, env, ctx)
-		}
-	}
+  return {
+    ...options,
+    fetch: (request, env, ctx) => {
+      const app = new Hono<Env>()
+      options.app(app)
+      return app.fetch(request, env, ctx)
+    }
+  }
 }
