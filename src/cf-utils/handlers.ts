efda2deaeb03a7ca449e0e486b29fb794ee4c002
Will Cullen <itzcull@outlook.com>
2025-06-08 14:42:13 +1000

    reorder generics

--- src/cf-utils/handlers.ts
+++ src/cf-utils/handlers.ts
@@ -1,14 +1,14 @@
 import { Hono } from 'hono'
 
-interface DefineHandlersOptions<Env, App extends Hono<Env>, QueueMessage> {
+interface DefineHandlersOptions<Env, QueueMessage = unknown, App extends Hono<Env> = Hono<Env>> {
   app: (app: Hono<Env>) => App
   scheduled?: ExportedHandlerScheduledHandler<Env>
   queue?: ExportedHandlerQueueHandler<Env, QueueMessage>
 }
 
 /** @description Only the 'fetch', 'queue', and 'scheduled' handlers can be provisioned together. */
-export function defineWorkerHandlers<Env, App extends Hono<Env> = Hono<Env>, QueueMessage = unknown, CfHostMetadata = unknown>(
-  options: DefineHandlersOptions<Env, App, QueueMessage>
+export function defineWorkerHandlers<Env, QueueMessage = unknown, App extends Hono<Env> = Hono<Env>, CfHostMetadata = unknown>(
+  options: DefineHandlersOptions<Env, QueueMessage, App>
 ): ExportedHandler<Env, QueueMessage, CfHostMetadata> {
   const { app, ...handlers } = options
   return {
