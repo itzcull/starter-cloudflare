3f5a54d209577d5a0e7d36438540a3e43147b8d5
Will Cullen <itzcull@outlook.com>
2025-10-22 20:48:01 +1100

    add testing setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
 import { reactRouter } from '@react-router/dev/vite'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
 	plugins: [
+		react(),
 		cloudflare({
 			viteEnvironment: {
 				name: 'ssr' // This turned out to be ridiculously important, I couldn't run 'vite preview' or 'wrangler deploy' without it
@@ -16,5 +18,11 @@ export default defineConfig({
 		}),
 		reactRouter(),
 		tsconfigPaths()
-	]
+	],
+	test: {
+		globals: true,
+		environment: 'happy-dom',
+		setupFiles: ['./vitest.setup.ts'],
+		css: true
+	}
 })
